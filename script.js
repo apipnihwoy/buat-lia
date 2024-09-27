291730bc6e0c66b54b65a0b2838789c08cf44553
apipnihwoy <rifai4lf1701@gmail.com>
2024-09-27 13:16:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert('ini bener lia kan?')
+alert('Amalia Putri Fadilla loh!')
+alert('soalnya ini cuma buat dia... eakk')
+alert('yaudah nih')
+onload = () => {
+    document.body.classList.remove("container");
+  };
